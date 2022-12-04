f7e776a9413c68baf46c2d22d6ac612543989498
Sam Wheating <samwheating@gmail.com>
2022-12-03 21:54:57 -0800

    Cleaning up day 4 solution

--- 4/solution.py
+++ 4/solution.py
@@ -1,38 +1,17 @@
 with open("input.txt") as ifp:
     rows = ifp.read().splitlines()
 
-score = 0
-for row in rows:
-
-    a, b = (int(x) for x in row.split(",")[0].split("-"))
-    c, d = (int(x) for x in row.split(",")[1].split("-"))
-   
-    # --a-----b--
-    # ----c--d--
-
-    if a <= c:
-        if b >= d:
-            score += 1
-            continue
-    if c <= a:
-        if d >= b:
-            score += 1
-
-
-print(f"Part 1: {score}")
-
-score = 0
+part1 = part2 = 0
 for row in rows:
 
     a, b = (int(x) for x in row.split(",")[0].split("-"))
     c, d = (int(x) for x in row.split(",")[1].split("-"))
 
-    if a <= c:
-        if b >= c:
-            score += 1
-    else:
-        if d >= a:
-            score +=1
+    if (a <= c and b >= d) or (c <= a and d >= b):
+        part1 += 1
 
+    if not (b < c or a > d):
+        part2 +=1 
 
-print(f"Part 2: {score}")
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
