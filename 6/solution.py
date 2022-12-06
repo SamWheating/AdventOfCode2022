311ef9abb6861068ea537dc8d32b909f86c5ca8e
Sam Wheating <samwheating@gmail.com>
2022-12-05 21:09:32 -0800

    Cleaning up day 6 solution

--- 6/solution.py
+++ 6/solution.py
@@ -1,32 +1,11 @@
 with open("input.txt") as ifp:
-    rows = ifp.read().splitlines()
+    datastream = ifp.read()
 
-# with open("test_input.txt") as ifp:
-#     rows = ifp.read().splitlines()
+def find_solution(datastream, n):
+    """find the index of the first character in datastream following n unique characters"""
+    for i in range(n,len(datastream)):
+        if len(set(datastream[i-n:i])) == n:
+            return i
 
-def parse_input(rows):
-    return rows
-
-
-# part 1
-input = parse_input(rows)
-row = rows[0]
-for i in range(4,len(row)):
-    if len(set(row[i-4:i])) == 4:
-        result = i
-        break
-
-
-
-print(f"Part 1 result: {result}")
-
-
-# part 2
-input = parse_input(rows)
-row = rows[0]
-for i in range(14,len(row)):
-    if len(set(row[i-14:i])) == 14:
-        result = i
-        break
-
-print(f"Part 2 result: {result}")
+print(f"Part 1 result: {find_solution(datastream, 4)}")
+print(f"Part 2 result: {find_solution(datastream, 14)}")
